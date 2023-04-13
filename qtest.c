87ee08cac18745802223e54fdd55c49c18432fa2
ctfish <a0936281757@gmail.com>
2023-04-13 15:38:41 +0800

    Add q_shuffle command in q_test
    Add in q_test a command to shuffle queue using Fisher-Yates shuffle.

--- qtest.c
+++ qtest.c
@@ -890,6 +890,44 @@ static bool do_merge(int argc, char *argv[])
     return ok && !error_check();
 }
 
+/* Shuffle queue using FIsher-Yates shuffle*/
+void q_shuffle(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head)) {
+        return;
+    }
+    int len = q_size(head);
+    for (int i = len - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        struct list_head *node = head->next;
+        for (int k = 0; k < j; k++) {
+            node = node->next;
+        }
+        list_move_tail(node, head);
+    }
+}
+
+static bool do_shuffle(int argc, char *argv[])
+{
+    if (argc != 1) {
+        report(1, "%s takes no arguments", argv[0]);
+        return false;
+    }
+
+    if (!current || !current->q)
+        report(3, "Warning: Calling shuffle on null queue");
+    error_check();
+
+    set_noallocate_mode(true);
+    if (exception_setup(true))
+        q_shuffle(current->q);
+    exception_cancel();
+
+    set_noallocate_mode(false);
+    q_show(3);
+    return !error_check();
+}
+
 static bool is_circular()
 {
     struct list_head *cur = current->q->next;
@@ -1065,6 +1103,9 @@ static void console_init()
                 "");
     ADD_COMMAND(reverseK, "Reverse the nodes of the queue 'K' at a time",
                 "[K]");
+    ADD_COMMAND(shuffle,
+                "Shuffle the nodes of the queue using Fisher-Yates shuffle",
+                "");
     add_param("length", &string_length, "Maximum length of displayed string",
               NULL);
     add_param("malloc", &fail_probability, "Malloc failure probability percent",
